9e8b136083274f34043a7aa47866cf65604c679b
LeonardoPrioriello <leonardo.prioriello@gmail.com>
2024-01-17 12:21:24 +0100

    login page added

--- Frontend_TT/src/app/home/home.page.ts
+++ Frontend_TT/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SelectMultipleControlValueAccessor } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {
+    setTimeout(function(){
+      router.navigate(['/login']);
+    }, 3000);
+  }
 
 }
---
+++ Frontend_TT/src/app/login/login.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { IonRouterOutlet, Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  // Variabili utilizzate per ricevere gli input dagli ion-input
+  userName: string = "";
+  password: string = "";
+
+  // Variabile utilizzata per la disattivazione del backButton dell'hardware
+  private backButtonSubscription: any;
+
+  constructor(private router: Router,
+    private alertController: AlertController,
+    private routerOutlet: IonRouterOutlet,
+    private platform: Platform) { }
+
+  ngOnInit() {}
+
+  ionViewCanLeave(): boolean {
+    return false;
+  }
+
+  // All'entrata nella pagina disattiviamo il backButton dell'hardware (Android) e lo swipe (iOS)
+  ionViewDidEnter() {
+    this.routerOutlet.swipeGesture = false;
+    this.backButtonSubscription = this.platform.backButton.subscribeWithPriority(100, () => { });
+  }
+
+  // All'uscita dalla pagina ripristiniamo il backButton dell'hardware (Android) e lo swipe (iOS)
+  ionViewDidLeave() {
+    this.routerOutlet.swipeGesture = true;
+    this.backButtonSubscription.unsubscribe();
+  }
+
+}
